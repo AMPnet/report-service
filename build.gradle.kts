c870595dc5ed0325593c3d130912521faa0410bd
Eugen Druzin <edruzin@gmail.com>
2020-08-17 12:16:52 +0200

    Initialize project

---
+++ build.gradle.kts
@@ -0,0 +1,109 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.text.SimpleDateFormat
+import java.util.Date
+
+plugins {
+    val kotlinVersion = "1.3.72"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+
+    id("org.springframework.boot") version "2.4.0-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("org.asciidoctor.convert") version "1.5.8"
+    id("org.jlleitschuh.gradle.ktlint") version "9.3.0"
+    id("io.gitlab.arturbosch.detekt").version("1.11.0")
+    idea
+    jacoco
+}
+
+group = "com.ampnet"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
+    maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.github.microutils:kotlin-logging:1.8.3")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "1.8"
+    }
+}
+
+jacoco.toolVersion = "0.8.5"
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        xml.destination = file("$buildDir/reports/jacoco/report.xml")
+        csv.isEnabled = false
+        html.destination = file("$buildDir/reports/jacoco/html")
+    }
+    sourceDirectories.setFrom(listOf(file("${project.projectDir}/src/main/kotlin")))
+    dependsOn(tasks.test)
+}
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.2".toBigDecimal()
+            }
+        }
+    }
+    mustRunAfter(tasks.jacocoTestReport)
+}
+
+detekt {
+    input = files("src/main/kotlin")
+}
+
+task("qualityCheck") {
+    dependsOn(tasks.ktlintCheck, tasks.detekt, tasks.jacocoTestReport, tasks.jacocoTestCoverageVerification)
+}
+
+tasks.asciidoctor {
+    attributes(
+        mapOf(
+            "snippets" to file("build/generated-snippets"),
+            "version" to version,
+            "date" to SimpleDateFormat("yyyy-MM-dd").format(Date())
+        )
+    )
+    dependsOn(tasks.test)
+}
+
+tasks.register<Copy>("copyDocs") {
+    from(file("$buildDir/asciidoc/html5"))
+    into(file("src/main/resources/static/docs"))
+    dependsOn(tasks.asciidoctor)
+}
---
+++ src/test/kotlin/com/ampnet/reportservice/ReportServiceApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.ampnet.reportservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReportServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
