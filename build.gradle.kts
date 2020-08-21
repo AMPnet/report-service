2799ead8837e91f9d774122394fe48e3484aea26
Eugen Druzin <edruzin@gmail.com>
2020-08-21 10:03:00 +0200

    Update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 plugins {
-    val kotlinVersion = "1.3.72"
+    val kotlinVersion = "1.4.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
 
