19bc176104a8209d5750bf823a823636d3e352a5
Eugen Druzin <edruzin@gmail.com>
2020-09-24 14:21:20 +0200

    Add openhtmlpdf logging

--- src/main/kotlin/com/ampnet/reportservice/config/OpenHtmlToPdfConfig.kt
+++ src/main/kotlin/com/ampnet/reportservice/config/OpenHtmlToPdfConfig.kt
@@ -1,9 +1,12 @@
 package com.ampnet.reportservice.config
 
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
+import com.openhtmltopdf.slf4j.Slf4jLogger
 import com.openhtmltopdf.svgsupport.BatikSVGDrawer
+import com.openhtmltopdf.util.XRLog
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import javax.annotation.PostConstruct
 
 @Configuration
 class OpenHtmlToPdfConfig {
@@ -11,5 +14,10 @@ class OpenHtmlToPdfConfig {
     @Bean
     fun pdfRenderer(): PdfRendererBuilder {
         return PdfRendererBuilder().useFastMode().useSVGDrawer(BatikSVGDrawer())
+    }
+
+    @PostConstruct
+    fun setLogging() {
+        XRLog.setLoggerImpl(Slf4jLogger())
     }
 }
